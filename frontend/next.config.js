443de41ee2b068edadb12502b79ec4807dd43352
akiogitgit <akimaron2828@icloud.com>
2022-10-14 11:16:14 +0900

    feat: next.jsを同期して開発

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,7 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-// ここから
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 800,
-      aggregateTimeout: 300,
-    }
-    return config
-  },
-// ここまで
+  swcMinify: true,
 }
+
+module.exports = nextConfig
