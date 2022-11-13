96f450e9aa299a6a017958821cca905557c59fb6
akiogitgit <akimaron2828@icloud.com>
2022-11-13 14:06:06 +0900

    feat: mysqlコンテナを追加で建てる

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-        Welcome <a href="https://nextjs.org">Next.js!</a>
+        WelWel<a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
